be336e8f816c73b0d15e5a9ec7fc604ecc18d379
Danila <67117334+danilakla@users.noreply.github.com>
2023-10-19 23:24:33 +0300

    more test

--- src/test/java/com/crazy_ataman/calculator/CalculatorTest.java
+++ src/test/java/com/crazy_ataman/calculator/CalculatorTest.java
@@ -3,7 +3,9 @@ package com.crazy_ataman.calculator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.api.Timeout;
 
+import java.util.concurrent.TimeUnit;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,6 +19,135 @@ class CalculatorTest {
         calculator = new Calculator();
     }
 
+      @Test
+    public void testAddition() {
+        String expression = "2+2";
+        Optional<Double> result = calculator.solve(expression);
+        assertTrue(result.isPresent());
+        assertEquals(4.0, result.get());
+    }
+
+    @Test
+    public void testSubtraction() {
+        String expression = "5-3";
+        Optional<Double> result = calculator.solve(expression);
+        assertTrue(result.isPresent());
+        assertEquals(2.0, result.get());
+    }
+
+    @Test
+    public void testMultiplication() {
+        String expression = "3*4";
+        Optional<Double> result = calculator.solve(expression);
+        assertTrue(result.isPresent());
+        assertEquals(12.0, result.get());
+    }
+  @Test
+    public void testNumberTokenCreation() {
+        CalculatorToken token = CalculatorToken.number(5.0, 1);
+        assertTrue(token.isNumber());
+        assertFalse(token.isOperation());
+        assertEquals(5.0, token.getNumber());
+        assertNull(token.getOperation());
+        assertEquals(1, token.getIndex());
+    }
+
+    @Test
+    public void testOperationTokenCreation() {
+        CalculatorToken token = CalculatorToken.operation('+', 2);
+        assertFalse(token.isNumber());
+        assertTrue(token.isOperation());
+        assertNull(token.getNumber());
+        assertEquals(Character.valueOf('+'), token.getOperation());
+        assertEquals(2, token.getIndex());
+    }
+
+    @Test
+    public void testEquals() {
+        CalculatorToken token1 = CalculatorToken.number(5.0, 1);
+        CalculatorToken token2 = CalculatorToken.number(5.0, 1);
+        CalculatorToken token3 = CalculatorToken.operation('+', 2);
+        
+        assertEquals(token1, token2);
+        assertNotEquals(token1, token3);
+        assertNotEquals(token2, token3);
+    }
+
+    @Test
+    public void testHashCode() {
+        CalculatorToken token1 = CalculatorToken.number(5.0, 1);
+        CalculatorToken token2 = CalculatorToken.number(5.0, 1);
+        CalculatorToken token3 = CalculatorToken.operation('+', 2);
+
+        assertEquals(token1.hashCode(), token2.hashCode());
+        assertNotEquals(token1.hashCode(), token3.hashCode());
+    }
+
+    @Test
+    public void testToString() {
+        CalculatorToken token = CalculatorToken.number(5.0, 1);
+        String expected = "CalculatorToken{number=5.0, operation=null, index=1}";
+        assertEquals(expected, token.toString());
+    }
+      @Test
+    public void testNumberTokenCreationWithNullValue() {
+        assertThrows(NullPointerException.class, () -> CalculatorToken.number(null, 1));
+    }
+
+    @Test
+    public void testOperationTokenCreationWithNullValue() {
+        assertThrows(NullPointerException.class, () -> CalculatorToken.operation(null, 2));
+    }
+
+    @Test
+    public void testTokenCreationWithNegativeIndex() {
+    
+        CalculatorToken token = CalculatorToken.number(5.0, -1);
+        assertTrue(token.getIndex() < 0);
+    }
+
+    @Test
+    @Timeout(value = 100, unit = TimeUnit.MILLISECONDS)
+    public void testTokenToStringPerformance() {
+        CalculatorToken token = CalculatorToken.number(5.0, 1);
+        token.toString();
+    }
+    @Test
+    public void testDivision() {
+        String expression = "8/2";
+        Optional<Double> result = calculator.solve(expression);
+        assertTrue(result.isPresent());
+        assertEquals(4.0, result.get());
+    }
+
+    @Test
+    public void testComplexExpression() {
+        String expression = "3+5*2";
+        Optional<Double> result = calculator.solve(expression);
+        assertTrue(result.isPresent());
+        assertEquals(13.0, result.get());
+    }
+
+    @Test
+    public void testInvalidExpression() {
+        String expression = "3+";
+        Optional<Double> result = calculator.solve(expression);
+        assertFalse(result.isPresent());
+    }
+ @Test
+    public void testSolveValidExpression() {
+        String expression = "2+2";
+        Optional<Double> result = calculator.solve(expression);
+        assertTrue(result.isPresent());
+        assertEquals(4.0, result.get(), 0.01);
+    }
+
+    @Test
+    public void testSolveInvalidExpression() {
+        String expression = "2+";
+        Optional<Double> result = calculator.solve(expression);
+        assertFalse(result.isPresent());
+    }
     @Test
     void testNotNullLineShouldNotTrow() {
         String line = "123+45";
